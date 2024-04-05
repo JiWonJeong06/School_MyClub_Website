47ea4029dc59775579f20baa9f531851e9a971db
JiWonJeong06 <jungjw409@gmail.com>
2024-04-05 16:14:32 +0900

    bug fix and activity abillity add .

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function Scroll(className) {
     const element = document.querySelector(className);
     element.scrollIntoView({ behavior: 'smooth'});
 }
-//이벤트 함수
 //웹페이지 함수
 
 function GoToPagedcoder() {
@@ -12,4 +11,10 @@ function GoToPagedcoder() {
 function GoToPageRank() {
     document.location.href  = "http://www.dcoder.kr/ranklist.php"
 }
+
+function GotoStore() {
+    document.location.href = "store.html"
+}
 //다운 함수
+
+//이벤트 함수
