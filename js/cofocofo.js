c10a424c5a0f085b29aad9a19bd59598df98ad01
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-06-11 16:28:06 +0900

    ui fix

--- js/cofocofo.js
+++ js/cofocofo.js
@@ -1,16 +1,20 @@
 //질문 (랜덤으로 뽑기)
 //질문과 답과 같은 지 확인하기
-//카운트 다운
 //점수 올리기
-function Question() {
+document.getElementById('randomButton').addEventListener('click', showRandomImage);
 
+
+function RandomQuestion() {
+    const input = document.getElementById('fileInput');
+    const files = input.files;
+
+    if (files.length > 0) {
+        alert('Please select some images first.');
+        return;}
+    
 }
 
 function Answer() {
-
-}
-
-function Count() {
 
 }
 
