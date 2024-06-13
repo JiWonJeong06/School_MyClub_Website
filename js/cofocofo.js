3f74c1714bb53e3c279df936fe0ba321456e4304
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-06-13 17:23:34 +0900

    start button add

--- js/cofocofo.js
+++ js/cofocofo.js
@@ -5,32 +5,44 @@ var score = document.getElementById('score');
 var Ranbutton = document.getElementById('randomButton');
 var InputDown = document.getElementById('InputDown');
 var Surren = document.getElementById('surrenderButton');
-var addscore = 0;
+var addscore = -1;
 
+var preview = document.getElementById('preview');
 
+var Questions = [
+    
+];
+Surren.disabled = true;
+InputDown.disabled = true;
 //버튼과 엔터를 눌렀을 때 상호 작용과 점수 올리기
-if (Ranbutton) {
+if (Ranbutton && InputDown) {
+    
     Ranbutton.addEventListener('click', function(){
     alert('버튼 클릭');
+    Ranbutton.textContent = "Enter";
     addscore++;
     score.textContent = '점수: ' + addscore;
+    Surren.disabled = false;
+    InputDown.disabled = false;
 });
+    InputDown.addEventListener('keydown', function(event) {
+    if(event.key == 'Enter') {
+        alert('Enter 동작 감지');
+        Ranbutton.textContent = "Enter";
+        addscore++;
+        score.textContent = '점수: ' + addscore;
+        Surren.disabled = false;
+    }
+});
+
 }
 
-if (InputDown) {
-    InputDown.addEventListener('keydown', function(event) {
-        if(event.key == 'Enter') {
-            alert('Enter 동작 감지');
-            addscore++;
-            score.textContent = '점수: ' + addscore;
-        }
-    });
-}
 
 
 //포기하고 다시하기 버튼
 if (Surren) {
     Surren.addEventListener('click', function(){
+    alert('게임 종료');
     score.textContent = '최종 점수: ' + addscore;
     Ranbutton.disabled = true;
     InputDown.disabled = true;
